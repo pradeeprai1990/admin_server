243d6d27cd670552f3a99c8fea78a2b3437e2b11
pradeeprai1990 <pradeep.9997@gmail.com>
2025-07-03 23:30:00 +0530

    latest Code

--- server/App/controllers/web/orderController.js
+++ server/App/controllers/web/orderController.js
@@ -1,19 +1,48 @@
 const orderModel = require("../../models/orderModels")
+const Razorpay = require('razorpay');
+var instance = new Razorpay({
+   key_id: 'rzp_test_WAft3lA6ly3OBc',
+   key_secret: '68E17CNWY8SemCvZ6ylOkuOY',
+ });
 
 let saveOrder=async (req,res)=>{
    let {paymentMethod}=req.body
-  
+   let obj={...req.body}
    if(paymentMethod==1){ //COD
-        let obj={...req.body}
+        
         obj['orderStatus']='process'
         await orderModel.insertOne(obj)
          //Delete Cart Items CartModel
-        res.send("Order Save")
+         
+        res.send({status:1,msg:"Order Save",paymentMethod})
        
    }
    else{ //Online
-        
-   }
+      //DB Order Create
+      obj['orderStatus']='pending' 
+      obj['paymentStatus']='1'
+      let orderData=await orderModel.insertOne(obj)
+      //RazorPay
+      //Amount 500*100,
+      //"currency": "INR",
+      //"receipt": orderData._id,
+
+      let orderObj={
+         "amount": req.body.orderAmount*100,
+         "currency": "INR",
+         "receipt": orderData._id
+      }
+
+      let ordersRes=await instance.orders.create(orderObj)
+
+      await orderModel.updateOne({_id:orderData._id},{$set:{
+         razorpayOrderId:ordersRes.id
+      }})
+
+     
+      res.send({status:1,msg:"Order Save",paymentMethod,ordersRes})
+
+   }  
 }
 
 module.exports={saveOrder}
